553985b0d2c7d79986047b285253003be9a66475
Mate Soos <mate.soos@ethereum.org>
2024-04-07 21:35:28 -0400

    Adding memory stuff

--- evaluate.sh
+++ evaluate.sh
@@ -7,41 +7,50 @@
 
 tools="--tools=\"hevm-bitwuzla,halmos,kontrol\""
 #
+memoutmb="16000"
 Help()
 {
    # Display Help
-   echo "Add description of the script functions here."
+   echo "NOTE: if you run with a >16 core machine, some things may NOT work"
+   echo "      because the max memory will still be 16GB, but e.g. HEVM"
+   echo "      will spin up more than 16 threads which may mem-out"
+   echo "      For these cases, increase memory to n*1000, where N is"
+   echo "      the core count of your chip"
+   echo ""
    echo
-   echo "./evaluate.sh [--smoke-test][--brief][--full]"
+   echo "./evaluate.sh [-m NUM][--smoke-test][--brief][--full]"
+   echo "-m NUM        Max total memory in MB. Default: $memoutmb"
    echo "--smoke-test  Check if installation is correct"
    echo "--brief       Check results by running for ~1h"
    echo "--full        Full run, takes ~24h"
    echo ""
 }
 
+
+
 Full () {
     echo "Full tests, needs 24h"
-    echo "You need 16GB of free memory for this to run."
-    todo="./bench.py --timeout 1000 $tools"
+    echo "You need $memoutmb of free memory for this to run."
+    todo="./bench.py -t 1000 $tools -m $memoutmb"
     echo "Running: $todo"
 }
 
 Brief () {
     echo "Full tests, needs 1h"
-    echo "You need 16GB of free memory for this to run."
-    todo="./bench.py --timeout 40 $tools"
+    echo "You need $memoutmb MB of free memory for this to run."
+    todo="./bench.py -t 40 $tools -m $memoutmb"
     echo "Running: $todo"
 }
 
 Smoketest () {
     echo "Smoke testing."
-    echo "You need 16GB of free memory for this to run."
-    todo="./bench.py --timeout 4 --test=whatever $tools"
+    echo "You need $memoutmb MB of free memory for this to run."
+    todo="./bench.py --timeout 4 --test=whatever $tools -m $memoutmb"
     echo "Running: $todo"
 }
 
 # Get the options
-while getopts ":hfbs-:" option; do
+while getopts ":hfbs-:m:" option; do
    case $option in
       h) # display Help
          Help
@@ -55,6 +64,9 @@ while getopts ":hfbs-:" option; do
       s)
          Smoketest
          exit;;
+      m)
+         memoutmb=${OPTARG}
+         ;;
       -)
           case "${OPTARG}" in
             help)
